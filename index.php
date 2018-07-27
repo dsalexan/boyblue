bc17a82b9729656fc9460d79c1282bb45f732cc7
dsalexan <danilo.salexan@gmail.com>
2018-07-26 23:34:07 -0300

    Reestruturacao, refatoracao do wall

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <!DOCTYPE html>
 <html lang="pt_BR">
 
-  <head>
+<head>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="dsalexan">
-    <meta name="author" content="taaaaaar">
 
     <title>ECTM Jr</title>
 
+    <link rel="icon" href="favicon.ico">
+
     <!-- Bootstrap core CSS -->
-    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="node_modules/bootstrap/dist/css/bootstrap-grid.min.css" rel="stylesheet">
+    <link href="node_modules/bootstrap/dist/css/bootstrap-reboot.min.css" rel="stylesheet">
+
+    <link href="node_modules/semantic-ui/dist/semantic.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="https://fonts.googleapis.com/css?family=Josefin+Slab:300,400,700" rel="stylesheet">
@@ -20,286 +25,21 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700|Merriweather:400,400i,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500" rel="stylesheet">
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
 
-  </head>
-
-  <body>
-
-    <header id="wall">
-      <div class="overlay centralize grad_grey_to_transparent">
-        <nav class="navbar navbar-expand-lg bg-faded">
-          <div class="container">
-            <ul class="navbar-nav">
-              <li class="nav-item active">
-                <a class="nav-link" href="#ectm-jr">ECTM Jr<span class="sr-only">(current)</span></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#projetos">PROJETOS</a>
-              </li>
-              <li class="nav-item brand">
-                <a class="nav-brand" href="http:\\www.ectmjr.com.br"><img class="img-fluid-height" src="images/ectm_logo.png"/></a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#competencias">COMPETÊNCIAS</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#eventos">EVENTOS</a>
-              </li>
-              <li class="nav-item nav-floating">
-                <a class="nav-link" href="#contato">CONTATO</a>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </header>
-
-    <section id="ectm-jr">
-      <div style="position: relative; height: 100%;">
-        <div class="centralize holder-thing" style="width: 20%; left: 0;">
-          <div class="section-title">
-            <p>EC</p>
-            <p>TM</p>
-            <p>Jr.</p>
-          </div>
-          <div class="title-border"></div>
-        </div>
-        <div class="centralize holder-thing" style="width: 80%; right: 0;">
-          <div>
-            <div class="block">
-              <div class="block-title"><div>Quem <span>Somos</span></div></div>
-              <div class="block-text">A "Empresa de Ciência e Tecnologia Multidisciplinar Júnior" é uma empresa júnior com foco em consultoria e eficiência multidisciplinar em projetos. Adotando a filosofia de um BI (Bacharelado Interdisciplinar) a razão da criação dessa empresa se mostrou na aplicação prática da multidisciplinaridade visando uma abordagem dos <b>seis eixos</b> adotados na graduação dos membros, intitulado Bacharelado em Ciência e Tecnologia. É a primeira empresa júnior da Universidade Federal de São Paulo - Campos São José dos Campos com atuação no mercado na forma de análise, composição, consultoria e criação de projetos multidisciplinares.</div>
-            </div>
-            <div class="block">
-              <div class="block-title"><div><span>Nossos</span> Valores</div></div>
-              <div class="block-text">
-                <div class="container">
-                  <div class="row" style="text-align: center">
-                    <div class="col-md-4">
-                      Empreendedorismo<br/>
-                      Profissionalismo<br/>
-                      Inovação<br/>
-                      Trabalho em equipe
-                    </div>
-                    <div class="col-md-4">
-                      Excelência<br/>
-                      Foco em resultados<br/>
-                      Cooperação<br/>
-                      Pro-atividade
-                    </div>
-                    <div class="col-md-4">
-                      Qualidade<br/>
-                      Ética<br/>
-                      Transparência<br/>
-                      Pontualidade
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="block">
-              <div class="block-title"><div><span>Nossa</span> Missão</div></div>
-              <div class="block-text">A principal missão da ECTM Jr. é cultivar valores, trocas de experiências, convivência com o futuro esperado e tornar a busca do conhecimento agradável</div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section id="projetos" class="quadro slider-inactive" data-slide=3 data-slide-size=3>
-      <div style="position: relative; height: 100%;">
-        <div class="centralize holder-thing" style="width: 20%; right: 0; background: #111;">
-          <div class="section-title">
-            <p>PRO</p>
-            <p>JE</p>
-            <p>TOS</p>
-          </div>
-          <div class="title-border"></div>
-        </div>
-        <div class="holder-thing" style="width: 80%; left: 0;">
-          <div class="boxes">
-            <div class="box left-25" href="#" target="_blank" data-index=2>
-              <div>
-                <div>
-                  <div class="box-title">Hair Studio</div>
-                  <div class="box-body">Desevolvimento do site para a Rede de Estágios, um projeto interno da ECTM Jr na UNIFESP, que visa aumentar as relações empresariais da universidade a fim de conceder mais oportunidades aos alunos da universidade.</div>
-                </div>
-              </div>
-            </div>
-            <div class="box left-50" href="#" target="_blank" data-index=1>
-              <div>
-                <div>
-                  <div class="box-title">Buffet Kids!</div>
-                  <div class="box-body">Desevolvimento do site para a Rede de Estágios, um projeto interno da ECTM Jr na UNIFESP, que visa aumentar as relações empresariais da universidade a fim de conceder mais oportunidades aos alunos da universidade.</div>
-                </div>
-              </div>
-            </div>
-            <a class="box left-75" href="http://rede.ectmjr.com.br" target="_blank" data-index=0>
-              <div>
-                <div>
-                  <div class="box-title">Rede de Estágios</div>
-                  <div class="box-body">Desevolvimento do site para a Rede de Estágios, um projeto interno da ECTM Jr na UNIFESP, que visa aumentar as relações empresariais da universidade a fim de conceder mais oportunidades aos alunos da universidade.</div>
-                </div>
-              </div>
-            </a>
-          </div>
-          <div class="slider">
-            <div id="box-prev" class="arrow inactive" style="text-align: left;" data-acc=1 data-status=1><</div>
-            <div id="box-next" class="arrow inactive" style="text-align: right;" data-acc=-1 data-status=0>></div>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <?php //include("clientes.php"); ?>
-
-    <section id="competencias" class="quadro" data-slide="1" data-size=4>
-      <div class="borda"></div>
-      <div class="overlay" style="position: relative; height: 100%;">
-        <div class="centralize holder-thing holder-title" style="width: 22.5%; right: 0;">
-          <div id="arrow-back" class="disabled"><i class="fa fa-arrow-left" aria-hidden="true"></i></div>
-          <div class="title-border"></div>
-          <div class="section-title">
-            <p>COM</p>
-            <p>PE</p>
-            <p>TEN</p>
-            <p>CIAS</p>
-          </div>
-        </div>
-        <div class="boxes">
-          <div class="comp-box" data-slide="1">
-            <div class="box-title">Sistemas e Sites</div>
-            <div class="box-next"></div>
-            <div class="box-body">
-              Design, conteúdo, e tecnologia na criação de sites para empresas. Cada empresa possui características únicas e funciona de uma determinada maneira, assim, a ECTM Jr busca desenvolver um sistema web personalizado, o qual atenda às necessidades e desejos dos nossos clientes, buscando a satisfação plena e o crescimento do seu negócio, fazendo com que o mesmo se destaque no mercado.
-            </div>
-          </div>
-          <div class="comp-box inactive" data-slide="2">
-            <div class="box-title">Aplicativos</div>
-            <div class="box-next"></div>
-            <div class="box-body">
-              A ECTM Jr., desenvolve aplicativos personalizados, de acordo com o público-alvo e objetivos, atende todo o tipo de segmento, desde pessoa física a grandes empresas. Nossa equipe especializada executa e entrega projetos completos para o seu negócio da forma a qual você quiser.
-            </div>
-          </div>
-          <div class="comp-box inactive" data-slide="3">
-            <div class="box-title">Identidade Visual</div>
-            <div class="box-next"></div>
-            <div class="box-body">
-              A ECTM JR., desenvolve toda a identidade visual do seu negócio, atuando na valorização da sua marca, a qual é composta principalmente pelo logotipo, o qual contém características as quais irá consolidar sua marca no mercado, fazendo que ela seja reconhecida pelo público e se destaque da demais empresas já existentes. Além disso, com sua marca em mãos, podemos realizar todos os outros serviços de comunicação e marketing, como a criação e produção de folders e folhetos, cartões de visita, materiais para divulgação, adesivos e rótulos, papelaria e entre outros.
-            </div>
-          </div>
-          <div class="comp-box inactive" data-slide="4">
-            <div class="box-title">Mídias Sociais</div>
-            <div class="box-next"></div>
-            <div class="box-body">
-              Tendo em mente a necessidade do mercado e a atualidade, sabemos o quanto as mídias sociais têm impactado para divulgação empresarial e manutenção do relacionamento com os clientes, dessa forma, a ECTM JR, gerencia os perfis da sua empresa com a finalidade de atingir o público-alvo, alavancar suas vendas e principalmente divulgar sua marca.
-            </div>
-          </div>
-        </div>
-        <div class="play-button 1disabled"><i class="fa fa-play" aria-hidden="true"></i></div>
-    </section>
+    <link href="css/index.css" rel="stylesheet">
 
 
-    <section id="eventos" class="quadro">
-      <div style="position: relative; height: 100%;">
-        <div class="centralize holder-thing" style="width: 20%; left: 0;">
-          <div class="title-border"></div>
-          <div class="section-title">
-            <p>E</p>
-            <p>VEN</p>
-            <p>TOS</p>
-          </div>
-        </div>
-        <div class="holder-thing" style="width: 80%; right: 0;">
-          <div style="height: 100%;">
-            <div href="#" target="_blank" class="event-box pic1" style="1background: #182227;">
-              <div class="after"></div>
-              <div class="box-title">III Semana Empresarial - Dia 2</div>
-              <div class="box-date">22/08/2017</div>
-            </div>
-            <div href="#" target="_blank" class="event-box pic2" style="1background: #1B2C34;">
-              <div class="after"></div>
-              <div class="box-title">III Semana Empresarial - Dia 3</div>
-              <div class="box-date">23/08/2017</div>
-            </div>
-            <div href="#" target="_blank" class="event-box pic3" style="1background: #223C46;">
-              <div class="after"></div>
-              <div class="box-title">III Semana Empresarial - Dia 4</div>
-              <div class="box-date">24/08/2017</div>
-            </div>
-            <div href="#" target="_blank" class="event-box pic4" style="1background: #254553;">
-              <div class="after"></div>
-              <div class="box-title">Efetivação dos Trainees 2017</div>
-              <div class="box-date">22/11/2017</div>
-            </div>
-          </div>
-        </div>
-    </section>
+</head>
+<body>
 
-    <section id="contato" class="quadro">
-      <div class="overlay"></div>
-      <div style="position: relative; height: 100%;">
-        <div class="centralize holder-thing" style="width: 30%; right: 0;">
-          <div class="title-border"></div>
-          <div class="section-title">
-            <p>CON</p>
-            <p>TA</p>
-            <p>TO</p>
-          </div>
-        </div>
-        <div class="holder-thing" style="width: 70%; left: 0;">
-          <div style="height: 100%; display: flex;">
-            <div class="form centralize">
-              <form name="contact-form" method="post" action="send-mail.php">
-                <input type="text" placeholder="Nome" name="nome"></input>
-                <input type="text" placeholder="E-mail" name="email"></input>
-                <input type="text" placeholder="Assunto" name="assunto"></input>
-                <textarea placeholder="Mensagem" name="mensagem" style="max-height:260px; min-height:55px;"></textarea>
-                <input type="submit" value="Enviar"></input>
-              </form>
-            </div>
-            <div class="info centralize">
-              <div>
-                <div class="text">Unidade Parque Tecnológico - Avenida Cesare Mansueto Giulio Lattes, n° 1201 - Eugênio de Mello, CEP: 12247-014</div>
-                <!--<div class="text">(TELEFONE)</div>-->
-                <div class="text">contato@ectmjr.com.br</div>
-                <div class="text">facebook.com/ectmjr</div>
-              </div>
-          </div>
-        </div>
-    </section>
-
-    <!-- Footer -->
-    <footer>
-      <div class="site-map centralize">
-        <div>
-          <div class="links">
-            <a class="link-active" href="#ectm-jr">ECTM Jr</a>
-            <a href="#projetos">Projetos</a>
-            <!--<a href="#clientes">Clientes</a>-->
-            <a href="#equipe">Equipe</a>
-            <a href="#eventos">Eventos</a>
-            <a href="#contato">Contato</a>
-            </ul>
-          </div>
-          <div class="copyright">&copy; 2017 ECTM Jr <span>-</span> BoyBlue Versão 0.3 α <span>-</span> Todos os direitos reservados</div>
-        </div>
-      </div>
-      <div class="follow-us centralize">
-        <div>
-          <div class="label">Nos siga:</div>
-          <div class="social-media">| |  | |  | |  | |</div>
-        </div>
-      </div>
-    </footer>
+    <?php include('views/wall.php'); ?>
 
     <!-- Bootstrap core JavaScript -->
-    <script src="jquery/jquery.min.js"></script>
-    <script src="bootstrap/js/bootstrap.bundle.min.js"></script>
-    <script src="js/general.js"></script>
+    <script src="node_modules/jquery/dist/jquery.min.js"></script>
 
-  </body>
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
+    <script src="node_modules/semantic-ui/dist/semantic.min.js"></script>
+
+</body>
 </html>
---
+++ views/wall.php
@@ -0,0 +1,28 @@
+<header id="wall">
+    <div class="overlay centralize grad_grey_to_transparent">
+        <nav class="navbar navbar-expand-lg bg-faded">
+            <div class="container">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#ectm-jr">ECTM Jr<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#projetos">PROJETOS</a>
+                    </li>
+                    <li class="nav-item brand">
+                        <a class="nav-brand" href="http:\\www.ectmjr.com.br"><img class="img-fluid-height" src="images/ectm_logo.png"/></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#competencias">COMPETÊNCIAS</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#eventos">EVENTOS</a>
+                    </li>
+                    <li class="nav-item nav-floating">
+                        <a class="nav-link" href="#contato">CONTATO</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>
